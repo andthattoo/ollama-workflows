d2fe7f347e8f9ba312bce66e488eeb900cd929ab
andthattoo <omer@firstbatch.xyz>
2024-06-12 22:24:58 +0300

    added workflow yml

--- examples/execute_workflow.rs
+++ examples/execute_workflow.rs
@@ -9,7 +9,7 @@ async fn main() {
     env_logger::Builder::from_env(env).init();
     let exe = Executor::new(Model::GPT3_5Turbo);
     let workflow = Workflow::new_from_json(
-        "/Users/kayaomers/Documents/firstbatch/ollama-workflows/my_workflows/research.json",
+        "./workflows/search.json",
     )
     .unwrap();
     let mut memory = ProgramMemory::new();
