d953b48991d2db54239219a3b95ec1eea10fd685
andthattoo <omer@firstbatch.xyz>
2024-06-13 18:03:47 +0300

    tests compiled to single file

---
+++ tests/run.rs
@@ -0,0 +1,41 @@
+use dotenv::dotenv;
+use env_logger::Env;
+use ollama_workflows::{Entry, Executor, Model, ProgramMemory, Workflow};
+
+#[tokio::test]
+async fn test_search_workflow() {
+    dotenv().ok();
+    let env = Env::default().filter_or("LOG_LEVEL", "info");
+    env_logger::Builder::from_env(env).init();
+    let exe = Executor::new(Model::Phi3Medium);
+    let workflow = Workflow::new_from_json("./tests/test_workflows/search.json").unwrap();
+    let mut memory = ProgramMemory::new();
+    let input = Entry::try_value_or_str("How would does reiki work?");
+    exe.execute(Some(&input), workflow, &mut memory).await;
+}
+
+#[tokio::test]
+async fn test_simple_workflow() {
+    dotenv().ok();
+    let env = Env::default().filter_or("LOG_LEVEL", "info");
+    env_logger::Builder::from_env(env).init();
+    let exe = Executor::new(Model::Phi3Medium);
+    let workflow = Workflow::new_from_json("./tests/test_workflows/simple.json").unwrap();
+    let mut memory = ProgramMemory::new();
+    let input = Entry::try_value_or_str("How would does reiki work?");
+    exe.execute(Some(&input), workflow, &mut memory).await;
+}
+
+#[tokio::test]
+async fn test_insert_workflow() {
+    dotenv().ok();
+    let env = Env::default().filter_or("LOG_LEVEL", "info");
+    env_logger::Builder::from_env(env).init();
+    let exe = Executor::new(Model::Phi3Medium);
+    let workflow = Workflow::new_from_json("./tests/test_workflows/insert.json").unwrap();
+    let mut memory = ProgramMemory::new();
+    let input = Entry::try_value_or_str("How would does reiki work?");
+    exe.execute(Some(&input), workflow, &mut memory).await;
+}
+
+
