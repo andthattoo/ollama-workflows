40d2135ebc6029ab4921a85a7c8e2d5519dd80b3
erhant <erhany96@gmail.com>
2024-06-13 19:00:23 +0300

    TryFrom string to model, pull or list model

--- tests/search_workflow.rs
+++ tests/search_workflow.rs
@@ -7,8 +7,13 @@ async fn test_search_workflow() {
     dotenv().ok();
     let env = Env::default().filter_or("LOG_LEVEL", "info");
     env_logger::Builder::from_env(env).init();
-    let exe = Executor::new(Model::GPT3_5Turbo);
-    let workflow = Workflow::new_from_json("./workflows/search.json").unwrap();
+
+    // create executor
+    let exe = Executor::new(Model::Phi3Mini);
+    exe.pull_model().await.expect("should pull model");
+
+    // execute workflow
+    let workflow = Workflow::new_from_json("./examples/workflows/search.json").unwrap();
     let mut memory = ProgramMemory::new();
     let input = Entry::try_value_or_str("How would does reiki work?");
     exe.execute(Some(&input), workflow, &mut memory).await;
